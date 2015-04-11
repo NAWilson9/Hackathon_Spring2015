243f0ae5f2997c67f48681d44f28c44021810e6e
Chris Helmick <chris.helmick93@gmail.com>
2015-04-11 00:39:55 -0500

    fake database added, seems to work

--- ServerSide/Server.js
+++ ServerSide/Server.js
@@ -1,6 +1,8 @@
 //Link dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var Database = require('fileDB.js').Database;
+var fs = require("fs");
 
 //Create server
 var server = express();
@@ -9,7 +11,20 @@ var server = express();
 server.use(bodyParser.json());
 server.use(express.static('../ClientSide/'));
 
+//server.get('/test', function(req, res){
+//
+//});
+
 //Start server
 server.listen(1337, function() {
     console.log('Hackathon server running on port ' + 1337);
+
+	// database test, shows it works, delete if you want
+	var test = Database('test');
+	test.put("hello", "world", function(){
+
+		test.get("hello", function(err, data){console.log(data)});
+
+	});
+
 });
